b4c72e08147958f7b19692b1a4caf24573ded30b
up1 <somkiat.p@gmail.com>
2024-01-10 01:08:32 +0700

    User data from context

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
-import { useAuthContext } from "../hooks/useAuth.js";
-
+import { useContext } from 'react';
+import { AuthContext } from "../contexts/AuthContext";
 export const Navbar = () => {
-  const { user, isLoggedIn, login, logout } = useAuthContext()
+
+  const authContext = useContext(AuthContext);
   return (
     <>
       <div className={'nav-container'}>
@@ -10,9 +11,9 @@ export const Navbar = () => {
         <nav id="sidebar" className={'nav-item-container'}>
           <NavLink to="/" className={"nav-item"}>Home</NavLink>
           <NavLink to="/game">Game</NavLink>
-          {isLoggedIn && <div>{user}</div>}
-          {isLoggedIn ? <button onClick={() => logout()}>Logout</button> :
-            <button onClick={() => login('demo_user')}>Login</button>}
+          {authContext.isLoggedIn && <div>{authContext.user}</div>}
+          {authContext.isLoggedIn ? <button onClick={() => authContext.logout()}>Logout</button> :
+            <button onClick={() => authContext.login('demo_user')}>Login</button>}
         </nav>
       </div>
       <hr />

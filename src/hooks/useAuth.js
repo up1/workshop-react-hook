684c4d63b5070d03a5095c5813683650632d7688
up1 <somkiat.p@gmail.com>
2024-01-10 01:32:50 +0700

    Working with local storage

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext.js";
 
 export const useAuth = () => useContext(AuthContext)
@@ -6,13 +6,25 @@ export const useAuth = () => useContext(AuthContext)
 export const useAuthContext = () => {
     const [user, setUser] = useState('')
     const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem('user_data'))
+        
+        if (userData) {
+            setUser(userData.user)
+            setIsLoggedIn(userData.isLoggedIn)
+        }
+    }, [])
+
     const login = (user) => {
         setUser(user)
         setIsLoggedIn(true)
+        localStorage.setItem("user_data", JSON.stringify({ user: user, isLoggedIn: true }))
     }
     const logout = () => {
         setUser('')
         setIsLoggedIn(false)
+        localStorage.removeItem("user_data")
     }
 
     return { user, isLoggedIn, login, logout }

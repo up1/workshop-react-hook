6166e3a547420fb406607472d6072939fd248099
up1 <somkiat.p@gmail.com>
2024-01-09 23:15:29 +0700

    Demo with useState

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import { Navbar } from "../components/Navbar.jsx";
+import { useState } from "react";
 
 export const HomePage = () => {
+  const [count, setCount] = useState(0)
   return (
     <>
       <Navbar />
       <h1>Welcome to my homepage</h1>
+
+      <h2>Count: {count}</h2>
+      <button onClick={() => {
+        setCount(count + 1)
+      }}>increase
+      </button>
+
+
       {/*  TODO: Cover Images */}
       {/*  TODO: Jokes */}
     </>
